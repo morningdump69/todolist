cd65a8064ac91dac6cd518e02b653ad9eb46a2a8
CN22 <codenation@CN22s-MacBook-Pro.local>
2020-02-19 14:13:22 +0000

    commit1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  state = {
+    list: [],
+    inputText: "",
+    errorText: "Please type something into the input box"
+  };
+
+  changeHandler = event => {
+    if (event.target.value === "") {
+      this.setState({
+        inputText: event.target.value,
+        errorText: "Please type something into the input box"
+      });
+    } else {
+      this.setState({
+        inputText: event.target.value,
+        errorText: ""
+      });
+    }
+  };
+
+  onSubmit = () => {
+    let storeInput = this.state.list;
+    storeInput.push(this.state.inputText);
+    this.setState({ list: storeInput, inputText: "" });
+  };
+
+  removeHandler = index => {
+    let temp = this.state.list;
+    temp.splice(index, 1);
+    this.setState({ list: temp });
+    console.log("test");
+  };
+
+  render() {
+    const isActive = this.state.inputText === "";
+    return (
+      <div className="Full">
+        <p>{this.state.errorText}</p>
+        <p>To Do List</p>
+        <input
+          className="boxInput"
+          type="text"
+          onChange={event => this.changeHandler(event)}
+          placeholder="give me a task"
+          value={this.state.inputText}
+        />
+        <button
+          className="button"
+          type="submit"
+          onClick={this.onSubmit}
+          disabled={isActive}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          add
+        </button>
+        <div className="list">
+          {this.state.list.map((savedInput, index) => {
+            return (
+              <p key={index} onclick={() => this.removeHandler(index)}>
+                {savedInput}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
