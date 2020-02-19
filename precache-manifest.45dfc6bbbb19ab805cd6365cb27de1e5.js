4329f6ffd2df7d5743ab4d0c31f41ce847647cea
CN22 <codenation@CN22s-MacBook-Pro.local>
2020-02-19 14:21:04 +0000

    Updates

---
+++ precache-manifest.45dfc6bbbb19ab805cd6365cb27de1e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b835a8d4dfc33088a8f3402ebc1e22",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "83ee92d6d00965e210ea",
+    "url": "/todolist/static/css/main.485ce76a.chunk.css"
+  },
+  {
+    "revision": "03a16db9efd842c9fe33",
+    "url": "/todolist/static/js/2.6e4fe870.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.6e4fe870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ee92d6d00965e210ea",
+    "url": "/todolist/static/js/main.8ccc98b4.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
